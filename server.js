2a1df103e92cd6b8f03c47f9b83d3fdb1a842f19
Jon Caveman <jon@joncaveman.com>
2024-09-29 14:51:47 -0400

    implemented file naming as well as mime type detection and proper file exts

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ import path from 'path';
 import jwt from 'jsonwebtoken'
 import { MongoClient } from 'mongodb';
 import { fileURLToPath } from 'url';
+import { fileTypeFromBuffer } from 'file-type';
+import { readChunk } from 'read-chunk';
 
 dotenv.config();
 
@@ -92,7 +94,9 @@ app.post('/api/generate', async (req, res) => {
       style: 'vivid',
       n: 1,
     });
-    
+    console.log('\n\n\n\n\n\n\n\n');
+    console.log(imageResponse.data[0]);
+    console.log('\n\n\n\n\n\n\n\n');
     await app.storeFileByUrl(imageResponse.data[0].url, req);
     res.send(imageResponse);
   } catch (error) {
@@ -164,6 +168,11 @@ app.post('/api/authenticate', async (req, res) => {
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+function getFileName(currentUserObj) {
+  // user ID + current time + random number
+  return `${currentUserObj._id}_${Date.now()}_${Math.floor(Math.random() * 99999999)}`;
+}
+
 // Called by storeFileByUrl to handle the actual saving of the file
 app.downloadFile = async (url, filePath) => {
   const response = await axios({
@@ -171,10 +180,22 @@ app.downloadFile = async (url, filePath) => {
     responseType: 'stream',
   });
 
+  // write file to disk
   return new Promise((resolve, reject) => {
     response.data.pipe(fs.createWriteStream(filePath))
-      .on('finish', () => resolve())
+      .on('finish', () => resolve(filePath))
       .on('error', (e) => reject(e));
+  }).then(async filePath => {
+    // read the first 4100 bytes of the file to determine the file type
+    const buffer = await readChunk(filePath, {length: 4100});
+    const fileMetadata = await fileTypeFromBuffer(buffer);
+
+    // rename the file on disk to include the file extension
+    fs.rename(filePath, `${filePath}.${fileMetadata.ext}`, (err) => {
+      if (err) {
+        console.log('Error renaming file:', err);
+      }
+    });
   });
 };
 
@@ -185,9 +206,9 @@ app.storeFileByUrl = async function (imageUrl, req) {
     return false;
   }
 
-  const fileName = path.basename(imageUrl);
-  const filePath = path.join(__dirname, SAVE_FILE_PATH, fileName + '.jpg');
-
+  const currentUserObj = JSON.parse(req.body.currentUser);
+  const fileName = getFileName(currentUserObj);
+  const filePath = path.join(__dirname, SAVE_FILE_PATH, fileName);
   try {
     // Ensure the downloads directory exists
     if (!fs.existsSync(path.join(__dirname, SAVE_FILE_PATH))) {
