2161918ef74ed1ca53e6ea0f20efa7c148b98b02
Jon Caveman <jon@joncaveman.com>
2024-10-07 16:43:04 -0400

    added subscribe endpoint to API

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ import dotenv from 'dotenv';
 import generateRouter from './routes/generate.js';
 import authenticateRouter from './routes/authenticate.js';
 import connectToDatabase from './services/MongoConnect.js';
+import { ObjectId } from 'mongodb';
+import e from 'express';
 
 dotenv.config();
 
@@ -22,10 +24,26 @@ app.use(bodyParser.json());
 app.use('/api/authenticate', authenticateRouter);
 app.use('/api/generate', generateRouter);
 
-// Creating the /test route
-app.get('/test', (req, res) => {
-  console.log('TEST ROUTE SUCCESSFUL');
-  res.send('API IS WORKING');
+app.post('/api/subscribe', async (req, res) => {
+  const user = await JSON.parse(req.body.currentUser);
+  try {
+    const existingUser = await mongoDb.collection('users').findOne({_id: new ObjectId(user._id)});
+    if (!existingUser) {
+      return res.status(404).send('User not found');
+    }
+
+    const subscribed = await mongoDb.collection('subscriptions').insertOne({
+      email: existingUser.email,
+      createdAt: Date.now(),
+      broadcast: new ObjectId('67009bb6561716ca36c62d69'),
+      owner: new ObjectId(user._id),
+    });
+
+    res.send({ success: true });
+  } catch (error) {
+    console.error('Error subscribing:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 export default app;
