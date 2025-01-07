05351c14cbdee32518f0f03d5ddac7574ce53510
Jon Caveman <jon@lareverie.studio>
2025-01-07 15:17:59 +0000

    updated pm2 config

--- api.config.cjs
+++ api.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
       env: {
         NODE_ENV: "prod",
         PORT: 3000,
+	BASE_URL: "https://api.4ads.ai",
         OPENAI_API_KEY: process.env.OPENAI_API_KEY,
         BUNNYCDN_APY_KEY: process.env.BUNNYCDN_APY_KEY,
       }
